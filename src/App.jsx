3e7af6644e0528ab9d23d1323124432dab6bf8ec
sothearah <sothearah168@gmail.com>
2025-03-05 13:08:44 +0700

    Sign in/up ForgotPassword (linking issues)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import ForgotPassword from "./components/signupAndSignin/ForgotPasswordComponent";
 import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
     <>
       <AppRoutes />
-      <ForgotPassword/>
     </>
   );
 }
