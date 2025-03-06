3ded6e9f98127b264a7ccf7dd7e7b92bfbe356ec
Kukseng <phokukseng@gmail.com>
2025-03-06 16:25:27 +0700

    push

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router";
 import MainLayout from "../layouts/MainLayout";
 import LoginForm from "../pages/auth/LoginFrom";
 import AllCoursePage from "../pages/contents/allcoursepage";
